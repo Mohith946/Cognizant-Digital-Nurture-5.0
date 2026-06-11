4b98a43ee4cca712ad5a7c48129e2a443b33cee5
Mohith L A <mohithla24@gmail.com>
2026-06-11 23:58:40 +0530

    Exercise2

---
+++ Exercise2/FactoryMethodPatternExample.java
@@ -0,0 +1,49 @@
+package Exercise2;
+
+interface Document {
+    void open();
+}
+
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word Document");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF Document");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel Document");
+    }
+}
+
+class DocumentFactory {
+    public Document createDocument(String type) {
+        if (type.equals("Word")) {
+            return new WordDocument();
+        } else if (type.equals("PDF")) {
+            return new PdfDocument();
+        } else if (type.equals("Excel")) {
+            return new ExcelDocument();
+        } else {
+            throw new IllegalArgumentException("Invalid document type");
+        }
+    }
+}
+
+class FactoryMethodPatternExample {
+    public static void main(String[] args) {
+        DocumentFactory factory = new DocumentFactory();
+        Document document = factory.createDocument("Word");
+        document.open();
+        document = factory.createDocument("PDF");
+        document.open();
+        document = factory.createDocument("Excel");
+        document.open();
+    }
+}
